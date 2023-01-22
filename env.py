9cda4a72cc96fc072d80dba177b661652a25488d
Shanbashir1 <shanbashir1@hotmail.com>
2023-01-22 14:54:03 +0000

    change setting to view media images

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 os.environ["DATABASE_URL"] = 'postgres://grqbckhj:frRBcw9fu_ARpQUU_HMyloDcy0piU1XX@rogue.db.elephantsql.com/grqbckhj'
 os.environ["SECRET_KEY"] = 'shan123waniah'
 os.environ["CLOUDINARY_URL"] = 'cloudinary://437478624657285:D_OXopFrbXGs3LSRcOvOXM-pKUM@dw6qbkciv'
+os.environ['DEV'] = '1'
